c409543db8536a814e2795f1df3949d1023ad20e
Emiliano Pelliccioni <emilianop11@gmail.com>
2023-01-28 14:39:08 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # solidity-struct-parser
 A module to parse solidity response structs to JSON
+
+This is a WIP that I need for a personal project.
+Tests and more features will be added later
---
+++ index.js
@@ -0,0 +1,36 @@
+exports.ParseSolidityStruct = (struct) => {
+    function removeNumberValues(obj) {
+        let numKeys = []
+        Object.keys(obj).forEach((key) => {
+            let isnum = /^\d+$/.test(key);
+            if (isnum) delete obj[key];
+
+            try {
+                let isarray = Array.isArray(obj[key]);
+                // this should be a bigint, so we cast it to string
+                if (!isarray) {
+                    obj[key] = JSON.parse(obj[key].toString())
+                }
+            } catch(e) {}
+        })
+
+        Object.keys(obj).forEach((key) => {
+            let isarray = Array.isArray(obj[key]);
+            if (isarray) {
+                obj[key] = obj[key].map((e) => {
+                    const contractObj = {...e}
+                    return removeNumberValues(contractObj)
+                })
+            }
+        })
+
+        return obj;
+    }
+
+    return struct.map((entry) => {
+        const contractObj = {...entry}
+        // remove keys in the object that are stringified numbers. Solidity auto includes them
+        return removeNumberValues(contractObj)
+    })
+   
+}
