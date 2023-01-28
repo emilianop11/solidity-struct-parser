1e61e9731a0120793c97881c353a66b4e16b1bcf
Emiliano Pelliccioni <emilianop11@gmail.com>
2023-01-28 19:29:59 -0300

    Bugfix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ exports.ParseSolidityStruct = (struct) => {
         Object.keys(obj).forEach((key) => {
             let isnum = /^\d+$/.test(key);
             if (isnum) delete obj[key];
-
+  
             try {
                 let isarray = Array.isArray(obj[key]);
                 // this should be a bigint, so we cast it to string
@@ -13,24 +13,29 @@ exports.ParseSolidityStruct = (struct) => {
                 }
             } catch(e) {}
         })
-
+  
         Object.keys(obj).forEach((key) => {
             let isarray = Array.isArray(obj[key]);
             if (isarray) {
                 obj[key] = obj[key].map((e) => {
-                    const contractObj = {...e}
-                    return removeNumberValues(contractObj)
+  
+                    if (typeof e === 'object') {
+                      const contractObj = {...e}
+                      return removeNumberValues(contractObj)
+                    } else {
+                      return e;
+                    }
                 })
             }
         })
-
+  
         return obj;
     }
-
+  
     return struct.map((entry) => {
         const contractObj = {...entry}
         // remove keys in the object that are stringified numbers. Solidity auto includes them
         return removeNumberValues(contractObj)
     })
    
-}
+  }
